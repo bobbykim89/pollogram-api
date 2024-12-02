7984035b601c0001aebf18d63c33546f6e83aae1
Sihun Kim <skim585@asu.edu>
2024-12-02 09:00:09 -0700

    feat(pollogram/urls): add static url patterns
    add static url patterns

--- pollogram/urls.py
+++ pollogram/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +27,4 @@ urlpatterns = [
     path('api/posts/', include('posts.urls')),
     path('api/comments/', include('comments.urls'))
     # path('api/user', include('user_profile.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
